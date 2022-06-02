03c9f35be9304d8217a6c06db103212c2115daae
Laray Lewis <LarayLewis11@gmail.com>
2022-06-02 12:56:27 -0600

    Defined Pokemon Variable Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'Rice Pudding';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Butterfree', type: ['flying', 'poison'], height:4}, 
+    {name: 'Charizard', type: ['fire', 'flying'], height:10}, 
+    {name: 'Starfish', type: 'water', height:3}
+];
