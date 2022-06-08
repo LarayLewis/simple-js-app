5c03c75780f0a733c438d1841c0da5517fcb2d74
LarayLewis <90478125+LarayLewis@users.noreply.github.com>
2022-06-08 14:53:26 -0600

    escaped object pokemon for troubleshooting

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
     {name: 'Butterfree', type: ['flying', 'poison'], height:4}, 
-    {name: 'Charizard', type: ['fire', 'flying'], height:10}, 
-    {name: 'Starfish', type: 'water', height:3}
+    //{name: 'Charizard', type: ['fire', 'flying'], height:10}, 
+    //{name: 'Starfish', type: 'water', height:3}
 ];
 //for (let i=0; i < pokemonList.length; i++) {
     document.write(pokemonList.name);
